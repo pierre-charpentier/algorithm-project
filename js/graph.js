3a82a203ed762ede05cdb67b8f60ba7380a5510b
Pierre Charpentier <charpentier.pierre@outlook.com>
2016-11-07 18:11:49 +0100

    Changes for dikjstra

--- js/graph.js
+++ js/graph.js
@@ -1,34 +1,64 @@
 function Graph() {
     this.vertices = [];
 
-    this.addVertex = function(vertexLabel) {
-        if (!this.vertices.hasOwnProperty(vertexId)) {
-            this.vertices.push()
-        }
+    this.addVertex = function (vertexLabel) {
+        this.vertices.push({ label: vertexLabel, successors: [] });
     };
 
-    this.removeVertex = function(vertexId) {
+    this.removeVertex = function (vertexId) {
         delete this.vertices[vertexId];
     };
 
-    this.addEdge = function(sourceVertexId, destinationVertexId, weight) {
-        if (!this.vertices[sourceVertexId].hasOwnProperty(destinationVertexId)) {
-            this.vertices[sourceVertexId][destinationVertexId] = [];
-        }
-
-        this.vertices[sourceVertexId][destinationVertexId].push(weight);
+    this.addEdge = function (sourceVertexId, destinationVertexId, weight) {
+        this.vertices[sourceVertexId - 1].successors[destinationVertexId - 1] = weight;
     };
 }
 
-Graph.prototype.dijkstra = function(sourceVertexLabel) {
-    var D = []; // undefined equal infinite
-    var E = [1];
+Graph.prototype.dijkstra = function (sourceVertexId) {
+    sourceVertexId--;
+    var N = this.vertices.length;
+    var D = [];
+    var E = [];
+    E[sourceVertexId] = true;
 
-    for (var i = 2; i < this.vertices.length; i++) {
-        D[i] = this.vertices[1][i];
+    for (var i = 1; i < N; i++) {
+        D[i] = this.vertices[sourceVertexId].successors[i] ||  Infinity;
     }
 
-    for (var i = 2; i < this.vertices.length; i++) {
-        
+    for (i = 1; i < N && findMinimumIndex() >= 0; i++) {
+        var t = findMinimumIndex();
+
+        E[t] = true;
+        this.vertices[t].successors.forEach(function (weight, vertexId) {
+            var m = D[t] + weight;
+
+            if(D[vertexId] > m) {
+                D[vertexId] = m;
+            }
+        });
+    }
+
+    return D;
+
+    function findMinimumIndex() {
+        // This is to pass already processed vertices.
+        var T = D.map(function (value, index) {
+            if (E[index]) {
+                value = Infinity;
+            }
+
+            return value;
+        });
+
+        var len = T.length,
+            min = Infinity,
+            index = -1;
+        while (len--) {
+            if (T[len] < min) {
+                min = T[len];
+                index = len;
+            }
+        }
+        return index;
     }
 };
